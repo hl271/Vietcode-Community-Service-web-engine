f84d4d208913c96f94ab2e49b8740ff613e4daac
Lan Dang <danghoanglan271@gmail.com>
2018-10-09 00:20:05 +0700

    Commit 09/10/2018 - Add Facebook Share to each post

--- src/main.js
+++ src/main.js
@@ -2,14 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import fb from './firebase'
+import VueMaterial from 'vue-material'
+import VueMeta from 'vue-meta'
+import SocialSharing from 'vue-social-sharing'
 
 import App from './App'
 
 import {store} from './store'
 import {router} from './router'
-import VueMaterial from 'vue-material'
 
 Vue.use(VueMaterial)
+Vue.use(VueMeta)
+Vue.use(SocialSharing)
 
 Vue.config.productionTip = false
 
--- src/pages/SinglePost.vue
+++ src/pages/SinglePost.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <div class="post-wrapper" v-if="singlePost !== null">
+            
             <h1>{{singlePost.info.title}}</h1>
+            
             <div v-html="singlePost.content"></div>
         </div>
         <h1></h1>
@@ -14,6 +16,34 @@ export default {
         singlePost() {
             return this.$store.state.singlePost
         }
+    },
+    metaInfo() {
+        return {
+            title: 'Post',
+            titleTemplate: ' %s | VCS Web Demo',
+            meta: [
+                {
+                    'property': 'og:title',
+                    'content': this.singlePost !== null ? this.singlePost.info.title : 'Loading' ,
+                    'vmid': 'og:title'
+                },
+                {
+                    'property': 'og:url',
+                    'content': `${window.location.hostname}${this.$route.fullPath}` ,
+                    'vmid': 'og:url'
+                },
+                {
+                    'property': 'og:description',
+                    'content':  this.singlePost !== null ? this.singlePost.info.short_description : 'Loading',
+                    'vmid': 'og:description'
+                },
+                {
+                    'property': 'og:image',
+                    'content':  this.singlePost !== null ? this.singlePost.displayImageURL : 'Loading',
+                    'vmid': 'og:image'
+                }
+            ]
+        }        
     }
 }
 </script>
